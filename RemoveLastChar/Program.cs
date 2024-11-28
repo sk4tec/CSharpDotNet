95143a887633b9573cc0ed1b1c79af2605c55e6a
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-11-28 16:29:42 +0000

    RevmoveLastCharacter

---
+++ RemoveLastChar/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string name = RemoveLastCharacter("SunjBob1");
+        Console.WriteLine(name);
+    }
+
+    static string RemoveLastCharacter(string value)
+    {
+        return value.Remove(value.Length - 1);
+    }
+}
