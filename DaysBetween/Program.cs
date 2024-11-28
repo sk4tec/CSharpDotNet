e7bda8ecb6eac3e236e9b52df5d2e515213eacff
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-11-28 16:24:12 +0000

    DaysBetween

---
+++ DaysBetween/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int days = DaysLeft2(DateTime.Now, DateTime.Now.AddDays(55));
+        Console.WriteLine(days);
+    }
+
+	static int DaysLeft(DateTime fromDate, DateTime toDate)
+	{
+        TimeSpan time =  toDate - fromDate;
+        return (int)time.TotalDays;
+	}
+
+    static int DaysLeft2(DateTime fromDate, DateTime toDate)
+	{
+		int days = (int)toDate.Subtract(fromDate).TotalDays;
+
+		if (days < 0)
+		{
+			days = 0;
+		}
+
+		return days;
+	}
+}
