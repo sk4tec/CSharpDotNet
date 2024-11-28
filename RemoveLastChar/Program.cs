1bb3fbeea9e3f7414f67579fc95c9833c2e05b4a
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-11-28 16:32:50 +0000

    cope with string empty or null

--- RemoveLastChar/Program.cs
+++ RemoveLastChar/Program.cs
@@ -2,7 +2,7 @@
 {
     static void Main(string[] args)
     {
-        string name = RemoveLastCharacter("SunjBob1");
+        string name = RemoveLastCharacter2("SunjBob1");
         Console.WriteLine(name);
     }
 
@@ -10,4 +10,14 @@
     {
         return value.Remove(value.Length - 1);
     }
+
+    static string RemoveLastCharacter2(string value)
+	{
+		if (string.IsNullOrWhiteSpace(value))
+		{
+			return value;
+		}
+
+		return value.Substring(0, value.Length - 1);
+	}
 }
