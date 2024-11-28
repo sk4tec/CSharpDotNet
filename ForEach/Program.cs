7198cb114430816e9fc71f7c24169f633f3921e4
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-11-28 13:30:07 +0000

    ForEach test

---
+++ ForEach/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("For Each, Yay!");
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+
+Console.WriteLine($"We have {sum} items in inventory.");
