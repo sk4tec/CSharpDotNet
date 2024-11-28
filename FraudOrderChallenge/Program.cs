8ea6098a8d8dd2eccd912d670a056a0199aa6b5a
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-11-28 13:42:03 +0000

    Add Fraud Order Challenge

---
+++ FraudOrderChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Fraud Order Challenge!");
+
+string[] orders = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string order in orders) 
+{
+    if (order.StartsWith('B'))
+    {
+        Console.WriteLine($"Reporting order as it starts with B {order}");
+    }
+}
