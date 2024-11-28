16d1202aa54f7232d0091fd99a2d06b8f8137250
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-11-28 16:48:37 +0000

    Speed conversion MPH to KPH

---
+++ SpeedConversion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var kph = ConvertToKph(100m);
+        Console.WriteLine(kph);
+    }
+
+    static decimal ConvertToKph(decimal mph)
+    {
+        return mph * 1.6093m;
+    }
+}
