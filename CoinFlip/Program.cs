f0196500f9a0cbb6f1eed5eb5324e06f1fd1d973
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-11-30 11:10:52 +0000

    Coin Toss

---
+++ CoinFlip/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Coin Flip with 50/50 chance!");
+
+var random = new Random();
+
+for (int i = 0; i < 1000; i++)
+{
+    var num = random.Next(0, 2); // note that 0 is inclusive and 2 is exclusive so its 0-1
+    var result = num == 0 ? 0 : num;
+    Console.WriteLine(num == 0 ? "heads" : "tails");
+}
