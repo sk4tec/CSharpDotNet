ffce838fc05a06359621771a3a158c83e4ba734d
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-11-30 10:32:55 +0000

    Conditional

---
+++ Conditional/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Conditionals");
+
+// Suppose you need to quickly determine whether a customer's purchase is eligible for a promotional discount.
+// The details for the promotion indicate that when a purchase value is greater than 1000 euros, the purchase
+// is eligible for a 100 euro discount. 
+// If the purchase amount is 1000 euros or less, the purchase is eligible for a 50 euro discount.
+
+int saleAmount = 1001;
+int discount = saleAmount > 1000 ? 100 : 50;
+
+Console.WriteLine($"{discount}");
