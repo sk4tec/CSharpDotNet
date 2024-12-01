84d0dfd78a11a60ec1624532988143de7dfc7ad4
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-12-01 09:35:15 +0000

    Scope
    how scope matters to local vars

---
+++ Scope/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Scope stuff!!");
+
+// Code sample 1
+bool flag = true;
+int value;
+
+if (flag) // as this is a variable
+{
+    value = 10;
+    Console.WriteLine($"Inside the code block: {value}"); // this line is possible
+}
+
+Console.WriteLine($"Outside the code block: {value}"); // this line is also possible, hence error of uninitialised value
+
+
+// Code sample 2
+int value;  // this variable is not initalised
+
+if (true)
+{
+    value = 10;
+    Console.WriteLine($"Inside the code block: {value}"); // this scopped var is initialised OK
+}
+
+Console.WriteLine($"Outside the code block: {value}"); // this var is not used NOT OK
