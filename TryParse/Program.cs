64cf54f279ced8ca41ed61a11d7e38bc5b215765
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-12-06 16:01:18 +0000

    Parsing

---
+++ TryParse/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Try Parse fun!");
+
+//string value = "102";
+string value = "bad";
+int result = 0;
+
+if (int.TryParse(value, out result))
+{
+   Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+   Console.WriteLine("Unable to report the measurement.");
+}
+Console.WriteLine($"Measurement (w/ offset): {50 + result}");
