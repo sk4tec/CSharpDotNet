380a382de63afdc8e317681995f83fe2571af94f
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-12-07 18:08:00 +0000

    Array fun... yay

---
+++ Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Arrays Fun... yay!");
+
+string[] pallets = [ "B14", "A11", "B12", "A13" ];
+
+Console.WriteLine("Sorted...");
+Array.Sort(pallets); //note that we call a static method on the Array class
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Console.WriteLine("Reversed...");
+
+Array.Reverse(pallets);
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
