fe2593c475551ec4c9584ac36934093de88e4845
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2024-12-12 10:22:09 +0000

    String challenge

---
+++ String Challenge/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("String challenge!");
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+var words = pangram.Split(" ");
+string[] output = new string[words.Length];
+
+// Use a for loop to keep track of the index
+for (int i = 0; i < words.Length; i++)
+{
+    var wordArray = words[i].ToCharArray();
+    Array.Reverse(wordArray);
+    output[i] = new string(wordArray);
+}
+
+string resultSentence = string.Join(" ", output);
+Console.WriteLine(resultSentence);
