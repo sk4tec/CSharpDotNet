1c98abaae17f018a4d531611f1df2eda46836ea4
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2025-01-07 15:44:20 +0000

    Formatting Strings

---
+++ FormattingStrings/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, 2025! Lets do some string formatting!");
+
+string first = "Hello";
+string second = "World";
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
+
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
